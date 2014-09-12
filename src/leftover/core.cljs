84fbcf480af357c3906dbc59bbe132cbbf2be32a
Nate Jones <nate@endot.org>
2014-09-11 21:17:29 -0700

    switch to channels for actions

--- src/leftover/core.cljs
+++ src/leftover/core.cljs
@@ -1,6 +1,8 @@
 (ns leftover.core
+  (:require-macros  [cljs.core.async.macros :refer  [go]])
   (:require [om.core :as om :include-macros true]
             [om-tools.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]
             [om-bootstrap.button :as obb]
             [om-bootstrap.input :as obi]
             [om-bootstrap.table :as obt]))
@@ -43,30 +45,45 @@
                ))))
 
 (defn enter-payment [app owner]
-  (reify om/IRender
-    (render [this]
+  (reify om/IRenderState
+    (render-state [this {:keys [actions]}]
       (dom/div {:class "col-sm-6 component"}
                (dom/form
                  (obi/input {:type "text" :placeholder "Location"})
                  (obi/input {:type "text" :placeholder "Amount"})
                  (obb/button-group {:justified? true}
-                                 (obb/button-group {} (obb/button { :bs-style "success" :on-click (fn [] (js/alert "Add")) } "Add"))))))))
+                                 (obb/button-group {} (obb/button { :bs-style "success" :on-click (fn [] (put! actions {:type "add-payment"})) } "Add"))))))))
 
 (defn button-bar [app owner]
-  (reify om/IRender
-    (render [this]
+  (reify om/IRenderState
+    (render-state [this {:keys [actions]}]
       (dom/div {:class "col-sm-6 component"}
                (obb/button-group {:justified? true}
-                                 (obb/button-group {} (obb/button { :bs-style "primary" :on-click (fn [] (om/transact! app :state (fn [_] "enter-payment"))) } "Enter Payment"))
-                                 (obb/button-group {} (obb/button { :bs-style "primary" :on-click (fn [] (om/transact! app :state (fn [_] "view-history"))) } "View History")))))))
+                                 (obb/button-group {} (obb/button { :bs-style "primary" :on-click (fn [] (put! actions {:type "enter-payment"})) } "Enter Payment"))
+                                 (obb/button-group {} (obb/button { :bs-style "primary" :on-click (fn [] (put! actions {:type "view-history"})) } "View History")))))))
 
 (defn main-view [app owner]
-  (reify om/IRender
-    (render [this]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:actions (chan)})
+    om/IWillMount
+    (will-mount [_]
+      (let [actions (om/get-state owner :actions)]
+        (go (loop []
+              (let [action (<! actions)]
+                (case (:type action)
+                  "enter-payment" (om/transact! app :state (fn [_] "enter-payment"))
+                  "view-history" (om/transact! app :state (fn [_] "view-history"))
+                  "add-payment" (.log js/console "add")
+                  )
+                (recur))))))
+    om/IRenderState
+    (render-state [this {:keys [actions]}]
       (dom/div
-        (om/build button-bar app)
+        (om/build button-bar app {:init-state {:actions actions}})
         (case (:state app)
-          "enter-payment" (om/build enter-payment app)
+          "enter-payment" (om/build enter-payment app {:init-state {:actions actions}})
           "view-history" (om/build view-history app)))))) 
 
 (om/root
